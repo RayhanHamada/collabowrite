3a92a757582a5ce03bbd54dfe3c4e712b9619767
RayhanHamada <hamadadev2016@gmail.com>
2021-05-06 01:27:06 +0700

    feat(api): add signup, signin and signout template for auth route

--- packages/api/lib/route/Auth.ts
+++ packages/api/lib/route/Auth.ts
@@ -1,5 +1,15 @@
 import { App } from '@tinyhttp/app';
 
-export const auth = new App().get('/', (_req, res) => {
-  res.end('/auth');
-});
+export const auth = new App()
+  .get('/', (_req, res) => {
+    res.end('/auth');
+  })
+  .post('/signup', (_req, _res) => {
+    // TODO: implement signup
+  })
+  .post('/signin', (_req, _res) => {
+    // TODO: implement signin
+  })
+  .post('signout', (_req, _res) => {
+    // TODO: implement signout
+  });
