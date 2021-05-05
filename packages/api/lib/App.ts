38b27c26389434c2d7c24dfa7e54d8d2ac690dca
RayhanHamada <hamadadev2016@gmail.com>
2021-05-06 02:08:12 +0700

    feat(api): apply cors

--- packages/api/lib/App.ts
+++ packages/api/lib/App.ts
@@ -1,9 +1,11 @@
 import { App } from '@tinyhttp/app';
 import { logger } from '@tinyhttp/logger';
+import { cors } from '@tinyhttp/cors';
 
 import route from './route';
 
 export const app = new App()
+  .use(cors())
   .use(
     logger({
       emoji: true,
