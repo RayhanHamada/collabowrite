45ea6f3aac117eddcf8181804247d64c007adf60
RayhanHamada <hamadadev2016@gmail.com>
2021-05-06 01:04:37 +0700

    feat(api): apply logger middleware

--- packages/api/lib/App.ts
+++ packages/api/lib/App.ts
@@ -1,5 +1,17 @@
 import { App } from '@tinyhttp/app';
+import { logger } from '@tinyhttp/logger';
 
-export const app = new App().get('/', (_req, res) => {
-  res.end(`You're at /`);
-});
+export const app = new App()
+  .use(
+    logger({
+      emoji: true,
+      output: {
+        callback: console.log,
+        color: true,
+      },
+      timestamp: true,
+    })
+  )
+  .get('/', (_req, res) => {
+    res.end(`You're at Home`);
+  });
