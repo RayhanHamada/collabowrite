e5128a8a79737e0e6c412002c8949979703b8226
RayhanHamada <hamadadev2016@gmail.com>
2021-05-08 23:49:02 +0700

    fix(api): change connectToDB connection string

--- packages/api/lib/MongoConnection.ts
+++ packages/api/lib/MongoConnection.ts
@@ -1,21 +1,26 @@
 import { connect } from 'mongoose';
 
 export async function connectToDB(): Promise<boolean> {
-  const connectionString = process.env.MONGO_URL;
-  let success = false;
+  const connectionString = process.env.MONGO_URL as string;
+  if (!connectionString) {
+    console.log(`connection string is empty`);
+    return false;
+  }
 
-  await connect('connectionString', {
+  let successConnecting = false;
+
+  await connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
     .then(() => {
       console.log(`connection to url ${connectionString} established !`);
-      success = true;
+      successConnecting = true;
     })
     .catch((err) => {
       console.log(`connection to url ${connectionString} not established !`);
       console.log(err);
     });
 
-  return success;
+  return successConnecting;
 }
