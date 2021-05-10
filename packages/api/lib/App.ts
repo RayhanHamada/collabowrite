571c75dbcf2ebd0adcf607718cd79a960b3b8f39
RayhanHamada <hamadadev2016@gmail.com>
2021-05-11 02:19:48 +0700

    chore(api): install fastify-cookie and fastify-jwt

--- packages/api/lib/App.ts
+++ packages/api/lib/App.ts
@@ -1,7 +1,15 @@
 import { fastify } from 'fastify';
+import { fastifyJWT } from 'fastify-jwt';
 
 import route from './route';
 
 export const app = fastify({ logger: true });
+
+app.register(fastifyJWT, {
+  secret: process.env.JWT_SECRET as string,
+  sign: {
+    expiresIn: '1hr',
+  },
+});
 
 app.register(route, { prefix: '/api' });
