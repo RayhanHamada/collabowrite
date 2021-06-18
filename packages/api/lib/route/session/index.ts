5bf663d7a1fe7f6f5fee7dd58c32079e35ac065f
RayhanHamada <hamadadev2016@gmail.com>
2021-06-19 00:22:01 +0700

    feat(api): remove auth and session route

--- packages/api/lib/route/session/index.ts
+++
@@ -1,8 +0,0 @@
-import { RouteFunc } from '../types';
-import { NewSessionHandler } from './New';
-
-export const SessionRoute: RouteFunc = (fastify, _opts, done) => {
-  fastify.post('/new', NewSessionHandler);
-
-  done();
-};
